12d15e4cfa24f5b9a9342e6d4a97810ae1e8e0d7
Cherie Lum <charismatic.cherie@gmail.com>
2018-03-05 18:29:44 -0500

    adding quiz

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 //which of the following are real mouse events?
 //mouseout, mousemove, and mouseover
 //NOT mouse click or mouseon
+
+//jQuery Event Listener Quiz 2
+$(the target element being listended to).on(the type of event being listened for, a function with the stuff we want to do)
+
+
+//jQuery to set up an event listener.
+// remove my-button and add success to body 
+$('#my-button').on('click',function() {
+    $('#my-button').remove(); 
+    $('body').addClass('success');
+});
